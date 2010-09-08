9068e96e8fd74cb29000d328e3c1f4aa6f335759
Kevin Griffin <griffin.kev@gmail.com>
2010-09-08 10:59:38 -0400

    first commit

---
+++ Classes/AutofillAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AutofillAppDelegate.h
+//  Autofill
+//
+//  Created by Kevin Griffin on 9/8/10.
+//  Copyright Chariot Solutions LLC 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AutofillAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ Classes/RegisterForm.h
@@ -0,0 +1,16 @@
+//
+//  RegisterForm.h
+//  Autofill
+//
+//  Created by Kevin Griffin on 9/8/10.
+//  Copyright 2010 Chariot Solutions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RegisterForm : UIViewController {
+
+}
+
+@end
